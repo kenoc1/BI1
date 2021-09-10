2eae05c89cf79fdbf7a3f65ff8151944b48129be
KECOLDEW <keno.coldewey@btc-ag.com>
2021-09-10 20:51:32 +0200

    retail

--- main.py
+++ main.py
@@ -3,51 +3,15 @@ from random import random, seed, gauss, randint, uniform
 import config
 import cx_Oracle
 import names
-from util import Util
+import util
 from db_service import DB
 
 con = cx_Oracle.connect(user=config.DB_CON_USER, password=config.DB_CON_PW, dsn=config.DB_CON_DSN, encoding="UTF-8")
 print("Database version:", con.version)
 
 
-def select_table(table_name):
-    try:
-        with con.cursor() as cursor:
-            # cursor.execute("""select * from ORTSKENNZAHL""")
-            cursor.execute(f"""select * from {table_name}""")
-            rows = cursor.fetchall()
-            if rows:
-                for row in rows:
-                    print(row)
-    except cx_Oracle.Error as error:
-        print('Error occurred:')
-        print(error)
-
-
-# 12,Kassierer*in
-# 13,Einkäufer
-# 14,Abteilungsleiter*in
-# 15,Oberkassierer*in
-# 16,Consultant
-# 17,Raumpfleger*in
-# 18,Praktikant*in
-# 19,Dualer Student*in
-# 20,Azubi
-# 21,Lagerist
-# 22,Filialleiter*in
-
 def insert_funktionen():
-    functions = ["Einkäufer",
-                 "Oberkassierer*in",
-                 "Consultant",
-                 "Raumpfleger*in",
-                 "Kassierer*in",
-                 "Praktikant*in",
-                 "Dualer Student*in",
-                 "Azubi",
-                 "Lagerist",
-                 "Abteilungsleiter*in",
-                 "Filialleiter*in"]
+    functions = config.FUNCTIONS
     try:
         with con.cursor() as cursor:
             sql = ('Insert into FUNKTION(BEZEICHNUNG)'
@@ -71,17 +35,17 @@ def insert_mitarbeiter_function():
                     worker_id = row[0]
                     salary = row[1]
                     if salary > 12000:
-                        function_id = _get_id_from_function("Filialleiter*in")
+                        function_id = db.get_id_from_function("Filialleiter*in")
                     elif salary > 6000:
-                        function_id = _get_id_from_function("Abteilungsleiter*in")
+                        function_id = db.get_id_from_function("Abteilungsleiter*in")
                     elif salary > 4000:
-                        mittel = [_get_id_from_function("Einkäufer"), _get_id_from_function("Raumpfleger*in"),
-                                  _get_id_from_function("Consultant")]
+                        mittel = [db.get_id_from_function("Einkäufer"), db.get_id_from_function("Raumpfleger*in"),
+                                  db.get_id_from_function("Consultant")]
                         function_id = mittel[randint(0, len(mittel) - 1)]
                     else:
-                        gering = [_get_id_from_function("Azubi"), _get_id_from_function("Oberkassierer*in"),
-                                  _get_id_from_function("Kassierer*in"), _get_id_from_function("Praktikant*in"),
-                                  _get_id_from_function("Dualer Student*in"), _get_id_from_function("Lagerist")]
+                        gering = [db.get_id_from_function("Azubi"), db.get_id_from_function("Oberkassierer*in"),
+                                  db.get_id_from_function("Kassierer*in"), db.get_id_from_function("Praktikant*in"),
+                                  db.get_id_from_function("Dualer Student*in"), db.get_id_from_function("Lagerist")]
                         function_id = gering[randint(0, len(gering) - 1)]
                     try:
                         with con.cursor() as cursor2:
@@ -109,7 +73,7 @@ def _insert_mitarbeiter_salary():
                         with con.cursor() as cursor2:
                             sql = ('UPDATE MITARBEITER SET "Gehalt" = salary WHERE MITARBEITER_ID = worker_id'
                                    'values(:salary,:worker_id)')
-                            cursor2.execute(sql, [_random_salary(), worker_id])
+                            cursor2.execute(sql, [util.random_salary(), worker_id])
                     except cx_Oracle.Error as error:
                         print('Error occurred in cursor 2:')
                         print(error)
@@ -123,8 +87,8 @@ def insert_mitarbeiter(range_number):
     address_id_min = db.get_address_id_min()
     address_id_max = db.get_address_id_max()
     for n in range(range_number):
-        _insert_mitarbeiter_row(_generate_firstname(), _generate_lastname(), _random_commission_rate(),
-                                _random_salary(), _random_address_id(address_id_min, address_id_max))
+        db.insert_mitarbeiter_row(util.generate_firstname(), util.generate_lastname(), util.random_commission_rate(),
+                                  util.random_salary(), _random_address_id(address_id_min, address_id_max))
 
 
 def insert_kunden(range_number):
@@ -132,102 +96,18 @@ def insert_kunden(range_number):
     address_id_min = db.get_address_id_min()
     address_id_max = db.get_address_id_max()
     for n in range(range_number):
-        _insert_kunde_row(_generate_firstname(), _generate_lastname(),
-                          _random_date('1/1/1970 1:30 PM', '1/1/2005 4:50 AM', random()),
-                          _random_address_id(address_id_min, address_id_max),
-                          _random_address_id(address_id_min, address_id_max))
-
-
-def _insert_mitarbeiter_row(first_name, last_name, commission_rate, salary, address_id):
-    sql = ('insert into MITARBEITER(VORNAME, NACHNAME, PROVISIONSSATZ, GEHALT, ADRESS_ID)'
-           'values(:first_name,:last_name,:commission_rate,:salary,:address_id)')
-    print("Mitarbeiter: ", [first_name, last_name, commission_rate, salary, address_id])
-    try:
-        # create a cursor
-        with con.cursor() as cursor:
-            # execute the insert statement
-            cursor.execute(sql, [first_name, last_name, commission_rate, address_id])
-            # commit work
-            con.commit()
-    except cx_Oracle.Error as error:
-        print('Error occurred:')
-        print(error)
-
-
-def _insert_kunde_row(first_name, last_name, birthdate, billing_address, shipping_address):
-    sql = ("insert into KUNDE(VORNAME, NACHNAME, GEBURTSDATUM, RECHNUNGS_ADRESSE_ID, LIEFER_ADRESSE_ID)"
-           "values(:first_name,:last_name, to_date(:birthdate,'MM/DD/YYYY HH:MI AM'),:billing_address,:shipping_address)")
-    print("Kunden: ", [first_name, last_name, birthdate, billing_address, shipping_address])
-    try:
-        # create a cursor
-        with con.cursor() as cursor:
-            # execute the insert statement
-            cursor.execute(sql, [first_name, last_name, birthdate, billing_address, shipping_address])
-            # commit work
-            con.commit()
-    except cx_Oracle.Error as error:
-        print('Error occurred:')
-        print(error)
-
-
-def _generate_firstname() -> str:
-    return names.get_first_name()
-
-
-def _generate_lastname() -> str:
-    return names.get_last_name()
-
-
-def _random_salary() -> float:
-    return uniform(300, 8000)
-
-
-def _random_commission_rate() -> float:
-    return round(uniform(0, 5), 2)
+        db.insert_kunde_row(util.generate_firstname(), util.generate_lastname(),
+                            util.random_date('1/1/1970 1:30 PM', '1/1/2005 4:50 AM', random()),
+                            _random_address_id(address_id_min, address_id_max),
+                            _random_address_id(address_id_min, address_id_max))
 
 
 def _random_address_id(start_address_id, end_address_id):
     present = False
     while not present:
         address_id = randint(start_address_id, end_address_id)
-        present = _address_present(address_id)
+        present = db.address_present(address_id)
     return address_id
-
-
-def _address_present(address_id):
-    with con.cursor() as cursor:
-        cursor.execute("""select * from ADRESSE WHERE ADRESS_ID = :address_id""", address_id=address_id)
-        row = cursor.fetchone()
-        if row:
-            return True
-        else:
-            False
-
-
-def _get_id_from_function(function_description):
-    try:
-        with con.cursor() as cursor:
-            cursor.execute("""select * from FUNKTION WHERE BEZEICHNUNG = :function_description""",
-                           function_description=function_description)
-            row = cursor.fetchone()
-        if row:
-            return row[0]
-    except cx_Oracle.Error as error:
-        print('Error occurred:')
-        print(error)
-
-
-def _str_time_prop(start, end, time_format, prop) -> str:
-    stime = time.mktime(time.strptime(start, time_format))
-    etime = time.mktime(time.strptime(end, time_format))
-
-    ptime = stime + prop * (etime - stime)
-
-    return time.strftime(time_format, time.localtime(ptime))
-
-
-def _random_date(start, end, prop) -> str:
-    return _str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
 
 
 def _testing():
@@ -251,13 +131,15 @@ def _testing():
     #                   _random_address_id(address_id_min, address_id_max))
     # _insert_mitarbeiter_row(_generate_firstname(), _generate_lastname(), _random_commission_rate(),
     #                         _random_address_id(address_id_min, address_id_max))
-    insert_mitarbeiter_function()
+    # insert_mitarbeiter_function()
     # _insert_mitarbeiter_salary()
     # insert_funktionen()
+    # insert_mitarbeiter(800)
+    # insert_mitarbeiter_function()
+    insert_kunden(3000)
 
 
 if __name__ == '__main__':
     print("Launching...")
-    util = Util()
     db = DB()
     _testing()
--- retail.py
+++ retail.py
@@ -48,8 +48,10 @@ db = None
 def import_retail_verkauf():
     with open('retail.csv', newline='') as csv_file:
         retail_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+        counter = 0
         for row in retail_reader:
-            # print(row)
+            counter += 1
+            print(counter)
             if row:
                 weight_products = []
                 count_products = []
@@ -64,36 +66,60 @@ def import_retail_verkauf():
                     tax_sum = 0
                     sale_sum = 0
                     weight_sum = 0
-                    # count_products_ids = []
-                    # weight_products_ids = []
                     if count_products:
                         for product in count_products:
-                            # ToDo: get Preis and add tax and sale sum
-                            # print(product)
                             weight_sum += product["BRUTTOGEWICHT"]
+                            price = db.select_current_sale_price_with_product_id(product["PRODUKT_ID"])
+                            sale_sum += price["BETRAG"]
+                            tax_sum += price["BETRAG"] * product["UMSATZSTEUERSATZ"] * 0.01
                     if weight_products:
                         for product in weight_products:
-                            # ToDo: get Preis and add tax and sale sum
                             # print(product)
                             count = uniform(0.1, 3)
                             product["PURCHASED_WEIGHT"] = product["NETTOGEWICHT"] * count
                             weight_sum += product["BRUTTOGEWICHT"] * count
+                            price = db.select_current_sale_price_with_product_id(product["PRODUKT_ID"])
+                            sale_sum += price["BETRAG"]
+                            tax_sum += price["BETRAG"] * product["UMSATZSTEUERSATZ"] * 0.01
 
                     worker_id = db.get_random_seller()
                     customer_id = db.get_random_customer()
+                    random_date_indicator = randint(1, 5)
+                    # 1 month
+                    if random_date_indicator == 3:
+                        sale_date = util.random_date('12/15/2020 1:30 PM', '1/15/2021 4:50 AM', random())
+                    # 1 month
+                    elif random_date_indicator == 2:
+                        sale_date = util.random_date('6/1/2021 1:30 PM', '6/30/2021 4:50 AM', random())
+                    # 3 month (execute if 1,4,5)
+                    else:
+                        sale_date = util.random_date('8/20/2021 1:30 PM', '11/30/2021 4:50 AM', random())
 
                     added_sale_id = db.insert_verkauf_row(
-                        sale_date=util.random_date('1/1/2019 1:30 PM', '1/1/2021 4:50 AM', random()),
-                        worker_id=worker_id, customer_id=customer_id, sale_sum=sale_sum, tax_sum=tax_sum,
-                        weight_sum=weight_sum)
-                    calculate_and_insert_count_products(added_sale_id, count_products)
-                    calculate_and_insert_weight_products(added_sale_id, weight_products)
+                        sale_date=sale_date, worker_id=worker_id, customer_id=customer_id, sale_netto_sum=sale_sum,
+                        sale_brutto_sum=sale_sum + tax_sum, tax_sum=tax_sum, weight_sum=weight_sum)
+                    db.calculate_and_insert_count_products_verkauf(added_sale_id, count_products)
+                    db.calculate_and_insert_weight_products_verkauf(added_sale_id, weight_products)
+                    if randint(0, 100) < 30:
+                        db.insert_delivery_note_row(sale_id=added_sale_id,
+                                                    delivery_date=util.generate_delivery_date(sale_date),
+                                                    delivery_costs=util.generate_delivery_costs())
+                        db.insert_invoice_row(sale_id=added_sale_id,
+                                              adjustment_date=util.generate_adjustment_date(sale_date))
+                    else:
+                        db.insert_bon_row(sale_id=added_sale_id,
+                                          given_money=util.generate_given_money(sale_sum + tax_sum),
+                                          payment_method=util.generate_payment_method(),
+                                          sale_sum=sale_sum + tax_sum)
 
 
 def import_retail_einkauf():
-    with open('retail.csv', newline='') as csv_file:
+    with open('retail1.csv', newline='') as csv_file:
         retail_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+        counter = 0
         for row in retail_reader:
+            counter += 1
+            print(counter)
             if row:
                 print(row)
                 weight_products = []
@@ -102,52 +128,27 @@ def import_retail_einkauf():
                     if db.product_present(product_id):
                         product = db.select_product_with_id(product_id)
                         if product["TYP"] == 'stueckbasiert':
-                            # ToDo: Add random count products
                             count_products.append(product)
                         elif product["TYP"] == 'gewichtsbasiert':
                             weight_products.append(product)
                 if count_products or weight_products:
-                    sale_sum = 0
-                    if count_products:
-                        for product in count_products:
-                            # ToDo: get sale sum
-                            print(product)
                     if weight_products:
                         for product in weight_products:
-                            # ToDo: get sale sum
-                            print(product)
                             count = uniform(3, 7)
                             product["PURCHASED_WEIGHT"] = product["NETTOGEWICHT"] * count
                 worker_id = db.get_random_buyer()
-                customer_id = db.get_random_customer()
+                supplier_id = db.get_random_supplier()
 
-                added_buying_id = db.insert_einkauf_row()
-                calculate_and_insert_count_products(added_buying_id, count_products)
-                calculate_and_insert_weight_products(added_buying_id, weight_products)
-
-
-def calculate_and_insert_count_products(sale_id, count_products):
-    count_products_ids = []
-    if count_products:
-        tmp_product_ids = []
-        for product in count_products:
-            tmp_product_ids.append(product["PRODUKT_ID"])
-        tmp_product_ids_counter = Counter(tmp_product_ids)
-        for product_id in tmp_product_ids_counter:
-            count_products_ids.append(
-                [sale_id, product_id, tmp_product_ids_counter[product_id]])
-        db.insert_verkauf_stueckbasiert(count_products_ids)
-
-
-def calculate_and_insert_weight_products(sale_id, weight_products):
-    weight_products_ids = []
-    if weight_products:
-        for product in weight_products:
-            weight_products_ids.append(
-                [sale_id, product["PRODUKT_ID"], product["PURCHASED_WEIGHT"]])
-        db.insert_verkauf_gewichtsbasiert(weight_products_ids)
+                added_buying_id = db.insert_einkauf_row(
+                    util.random_date('1/1/2017 1:30 PM', '1/1/2019 4:50 AM', random()),
+                    worker_id, supplier_id)
+                db.calculate_and_insert_count_products_einkauf(added_buying_id, count_products)
+                db.calculate_and_insert_weight_products_einkauf(added_buying_id, weight_products)
 
 
 db = DB()
+# x = db.select_current_sale_price_with_product_id(2324)
+# y = db.select_current_buying_price_with_product_id(2324)
+# print(x["BETRAG"])
 import_retail_verkauf()
 # import_retail_einkauf()
