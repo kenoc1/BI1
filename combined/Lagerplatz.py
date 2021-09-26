cd5b1c123e6ddcc85645ba1eb1fdfcea3ec21828
Sven <sven.meiners@student.ibs-ol.de>
2021-09-26 18:54:08 +0200

    Erstellen des Insert Statements

--- combined/Lagerplatz.py
+++ combined/Lagerplatz.py
@@ -13,63 +13,82 @@ class Lagerplatz:
                                         encoding="UTF-8")
 
         self.con_combined = cx_Oracle.connect(user=config.DB_CON_USER_COMBINED, password=config.DB_CON_PW_COMBINED,
-                                        dsn=config.DB_CON_DSN_F2,
-                                        encoding="UTF-8")
-
+                                              dsn=config.DB_CON_DSN_F2,
+                                              encoding="UTF-8")
+        self.importliste = []
 
     def getLagerplaetzeF2(self):
-
+        # nur die Verkaufsflächen
+        # Lagerflächen
         try:
             with self.con_f2.cursor() as cursor:
-                cursor.execute("""select LAGERPLATZ_ID,REGALZEILE,REGAL_NUMMER,REGALSPALTE from LAGER_EINHEIT """)
-                datalist_F2 = cursor.fetchall()
-                print(datalist_F2)
+                cursor.execute(
+                    """select ZF2.PRODUKT_ID, LF2.REGALZEILE,LF2.REGAL_NUMMER,LF2.REGALSPALTE from LAGER_EINHEIT LF2,ZUWEISUNG_PRODUKT_LAGERPLATZ ZF2 WHERE LF2.LAGERPLATZ_ID = ZF2.LAGERPLATZ_ID AND LF2.TYP = 'Verkaufsflaeche' """)
+                lagerplatzListe = cursor.fetchall()
+                return lagerplatzListe
 
         except cx_Oracle.Error as error:
             print('Error occurred:')
             print(error)
-
-
 
     def getAnzahlVerkaufe(self, produktID):
         try:
             with self.con_f2.cursor() as cursor:
-                cursor.execute(f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_VERKAUF WHERE PRODUKT_ID = {produktID}""")
+                cursor.execute(
+                    f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_VERKAUF WHERE PRODUKT_ID = {produktID}""")
                 anzahlProdukte = cursor.fetchall()
-                return anzahlProdukte
+                return anzahlProdukte[0][0]
 
         except cx_Oracle.Error as error:
             print('Error occurred:')
             print(error)
-
 
     def getAnzahlEinkaufe(self, produktID):
         try:
             with self.con_f2.cursor() as cursor:
-                cursor.execute(f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_EINKAUF WHERE PRODUKT_ID = {produktID}""")
+                cursor.execute(
+                    f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_EINKAUF WHERE PRODUKT_ID = {produktID}""")
                 anzahlProdukte = cursor.fetchall()
-                return anzahlProdukte
+                return anzahlProdukte[0][0]
 
         except cx_Oracle.Error as error:
             print('Error occurred:')
             print(error)
 
+    def getProduktIDsF2(self):
+        # anpassen da die neuen ProduktIDs in der neuen Datenbank benoetigt werden
+        # wir brauchen eine mapping tabeelle bei dcen produkten damit die Produkte exakt zugeordnet werden können
+        try:
+            with self.con_f2.cursor() as cursor:
+                cursor.execute("""select PRODUKT_ID from PRODUKT """)
+                produktlist = cursor.fetchall()
+                return produktlist
+        except cx_Oracle.Error as error:
+            print('Error occurred:')
+            print(error)
+
+    def berechneMenge(self, produkt):
+
+        einkauefe = self.getAnzahlEinkaufe(produkt)
+        verkaeufe = self.getAnzahlVerkaufe(produkt)
+
+        aktuelleMenge = einkauefe - verkaeufe
+        return aktuelleMenge
 
 
-
-
-
-
-
-
-
-
-
-
-id= 2001
+# zusammebauen der eigentlichen Insert liste
 
 object = Lagerplatz()
 
-print(object.getAnzahlEinkaufe(id))
-print(object.getAnzahlVerkaufe(id))
+produktliste = object.getProduktIDsF2()
+lagerplatzListe = object.getLagerplaetzeF2()
+
+i = 0
+for lagerplatz in lagerplatzListe:
+    lagerplatz_ = [31, lagerplatz[0], lagerplatz[1], lagerplatz[2], lagerplatz[3],
+                   object.berechneMenge(lagerplatz[0])]
+    object.importliste.extend(lagerplatz_)
+    i = i +1
+    print(lagerplatz_)
+
 
