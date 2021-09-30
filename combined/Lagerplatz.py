034e258e2f7e6e1e4d532e0ae0284c678c4df1b0
semeiner <sven.meiners@gmx.de>
2021-09-30 15:27:19 +0200

    Hinzufügen der Lager.py und anpassen der Import Statements

--- combined/Lagerplatz.py
+++ combined/Lagerplatz.py
@@ -1,5 +1,6 @@
 import cx_Oracle
 import config
+import random
 
 
 class Lagerplatz:
@@ -8,22 +9,18 @@ class Lagerplatz:
         self.con_f2 = cx_Oracle.connect(user=config.DB_CON_USER_F2, password=config.DB_CON_PW_F2,
                                         dsn=config.DB_CON_DSN_F2,
                                         encoding="UTF-8")
-        self.con_OS = cx_Oracle.connect(user=config.DB_CON_USER_OS, password=config.DB_CON_PW_OS,
-                                        dsn=config.DB_CON_DSN_F2,
-                                        encoding="UTF-8")
 
         self.con_combined = cx_Oracle.connect(user=config.DB_CON_USER_COMBINED, password=config.DB_CON_PW_COMBINED,
                                               dsn=config.DB_CON_DSN_F2,
                                               encoding="UTF-8")
         self.importliste = []
 
-    def getLagerplaetzeF2(self):
-        # nur die Verkaufsflächen
-        # Lagerflächen
+    def getLagerplaetzeF2(self, typ):
+        # gibt Liste mit ProduktID_F2 und weiteren Attributen zurück
         try:
             with self.con_f2.cursor() as cursor:
                 cursor.execute(
-                    """select ZF2.PRODUKT_ID, LF2.REGALZEILE,LF2.REGAL_NUMMER,LF2.REGALSPALTE from LAGER_EINHEIT LF2,ZUWEISUNG_PRODUKT_LAGERPLATZ ZF2 WHERE LF2.LAGERPLATZ_ID = ZF2.LAGERPLATZ_ID AND LF2.TYP = 'Verkaufsflaeche' """)
+                    f"""select ZF2.PRODUKT_ID, LF2.REGALZEILE,LF2.REGAL_NUMMER,LF2.REGALSPALTE from LAGER_EINHEIT LF2,ZUWEISUNG_PRODUKT_LAGERPLATZ ZF2 WHERE LF2.LAGERPLATZ_ID = ZF2.LAGERPLATZ_ID AND LF2.TYP = '{typ}'""")
                 lagerplatzListe = cursor.fetchall()
                 return lagerplatzListe
 
@@ -31,13 +28,13 @@ class Lagerplatz:
             print('Error occurred:')
             print(error)
 
-    def getAnzahlVerkaufe(self, produktID):
+    def getAnzahlVerkaufe(self, produktID_F2):
         try:
             with self.con_f2.cursor() as cursor:
                 cursor.execute(
-                    f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_VERKAUF WHERE PRODUKT_ID = {produktID}""")
-                anzahlProdukte = cursor.fetchall()
-                return anzahlProdukte[0][0]
+                    f"""select sum(anzahl_Produkte) from STUECKZAHLBASIERTES_PRODUKT_IM_VERKAUF WHERE PRODUKT_ID = {produktID_F2}""")
+                anzahlVerkaufe = cursor.fetchall()
+                return anzahlVerkaufe[0][0]
 
         except cx_Oracle.Error as error:
             print('Error occurred:')
@@ -47,48 +44,65 @@ class Lagerplatz:
         try:
             with self.con_f2.cursor() as cursor:
                 cursor.execute(
-                    f"""select count(PRODUKT_ID) from STUECKZAHLBASIERTES_PRODUKT_IM_EINKAUF WHERE PRODUKT_ID = {produktID}""")
-                anzahlProdukte = cursor.fetchall()
-                return anzahlProdukte[0][0]
+                    f"""select sum(anzahl_Produkte) from STUECKZAHLBASIERTES_PRODUKT_IM_EINKAUF WHERE PRODUKT_ID = {produktID}""")
+                anzahlEinkaufe = cursor.fetchall()
+                return anzahlEinkaufe[0][0]
 
         except cx_Oracle.Error as error:
             print('Error occurred:')
             print(error)
 
-    def getProduktIDsF2(self):
-        # anpassen da die neuen ProduktIDs in der neuen Datenbank benoetigt werden
-        # wir brauchen eine mapping tabeelle bei dcen produkten damit die Produkte exakt zugeordnet werden können
-        try:
-            with self.con_f2.cursor() as cursor:
-                cursor.execute("""select PRODUKT_ID from PRODUKT """)
-                produktlist = cursor.fetchall()
-                return produktlist
-        except cx_Oracle.Error as error:
-            print('Error occurred:')
-            print(error)
+    #def getProductMappingList(self, lagerplatzliste):
+        #anpassen da die neuen ProduktIDs in der neuen Datenbank benoetigt werden
+        # wir brauchen eine mapping tabelle bei dcen produkten damit die Produkte exakt zugeordnet werden können
+        #ändere productID in der Liste gegen neue
 
     def berechneMenge(self, produkt):
-
+        #hier noch gewichtprodukte berücksichtigen
         einkauefe = self.getAnzahlEinkaufe(produkt)
         verkaeufe = self.getAnzahlVerkaufe(produkt)
 
+        if einkauefe == None:
+            return int(random.random() * 20 + 1)
+
         aktuelleMenge = einkauefe - verkaeufe
+        if aktuelleMenge < 0:
+            aktuelleMenge = int(random.random() * 20 + 1)
         return aktuelleMenge
 
+    def insertLagerplaetze(self, lager_ID, produkt_ID, regal_reihe, ragel_spalte, akt_menge, regal_zeile):
+        try:
+            # add new Verkaufsfläche as column in combined DB
+            with self.con_combined.cursor() as cursor:
+                cursor.execute(f"""insert into LAGERPLATZ (LAGER_ID, PRODUKT_ID, REGAL_REIHE, REGAL_SPALTE, AKT_MENGE, REGAL_ZEILE)
+                                    values ({lager_ID}, {produkt_ID}, {regal_reihe}, {ragel_spalte}, {akt_menge}, {regal_zeile})""")
+                self.con_combined.commit()
+                print("Lagerplatz hinzugefuegt:" + lager_ID, produkt_ID, regal_reihe, ragel_spalte, akt_menge, regal_zeile)
 
-# zusammebauen der eigentlichen Insert liste
+        except cx_Oracle.Error as error:
+            print('Error occurred:')
+            print(error)
 
-object = Lagerplatz()
 
-produktliste = object.getProduktIDsF2()
-lagerplatzListe = object.getLagerplaetzeF2()
+
+lagerplatzobjekt = Lagerplatz()
+lagerplatzListe_Verkaufsflaeche = lagerplatzobjekt.getLagerplaetzeF2("Verkaufsflaeche")
+lagerplatzListe_Lagerflaeche = lagerplatzobjekt.getLagerplaetzeF2("Lagerflaeche")
 
 i = 0
-for lagerplatz in lagerplatzListe:
-    lagerplatz_ = [31, lagerplatz[0], lagerplatz[1], lagerplatz[2], lagerplatz[3],
-                   object.berechneMenge(lagerplatz[0])]
-    object.importliste.extend(lagerplatz_)
-    i = i +1
-    print(lagerplatz_)
+for lagerplatz in lagerplatzListe_Verkaufsflaeche:
+    temp = [31, lagerplatz[0], lagerplatz[1], lagerplatz[2], lagerplatz[3],
+                   lagerplatzobjekt.berechneMenge(lagerplatz[0])]
+
+    lagerplatzobjekt.importliste.append(temp)
+    i = i + 1
+    print(temp)
 
 
+for lagerplatz in lagerplatzListe_Lagerflaeche:
+    temp2 = [31, lagerplatz[0], lagerplatz[1], lagerplatz[2], lagerplatz[3],
+                   lagerplatzobjekt.berechneMenge(lagerplatz[0])]
+
+    lagerplatzobjekt.importliste.append(temp2)
+    i = i + 1
+    print(temp2)
