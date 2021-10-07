02c9535d2b7b5ebb9743b146e784087f3dce0729
Tobias <Telu8OjHyO2sWcnBSjfg>
2021-10-07 23:13:17 +0200

    fix(rudolf-db): Aktualisiere constraints

--- rudolf/sqlite_service.py
+++ rudolf/sqlite_service.py
@@ -1,83 +1,98 @@
 import sqlite3 as sl
 
+from rudolf import config
+
 
 class SQLiteService:
 
     def __init__(self):
         self.con = sl.connect('rudolf-db.db')
-        self.setup()
+        self._setup()
 
-    def setup(self):
+    def insert_id_allocation(self, table_name: str, new_id: int, old_id: int):
+        sql = ''' insert into {} (comb_id, f2_id) values (?,?);'''.format(table_name)
+        with self.con:
+            cursor = self.con.cursor()
+            cursor.execute(sql, (new_id, old_id))
+
+    def select_where_old_id(self, table_name: str, old_id: int):
+        sql = ''' select * from {} where f2_id = {}'''.format(table_name, old_id)
+        with self.con:
+            cursor = self.con.cursor()
+            cursor.execute(sql)
+            return cursor.fetchone()
+
+    def _setup(self):
         try:
             with self.con:
                 self.con.execute("""
                             CREATE TABLE if not exists adresse (
                                 adresse_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists bestellung (
                                 bestellung_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists einkauf (
                                 einkauf_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists lieferant_hersteller (
                                 lieferant_hersteller_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists marke (
                                 marke_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists produkte (
                                 produkte_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists kunden (
-                                produkte_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                kunden_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists mitarbeiter (
                                 mitarbeiter_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists subkategorien (
                                 subkategorien_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
                 self.con.execute("""
                             CREATE TABLE if not exists zwischenhaendler (
                                 zwischenhaendler_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                                 comb_id INTEGER,
-                                f2_id INTEGER
+                                f2_id INTEGER UNIQUE
                             );
                         """)
         except sl.OperationalError as oe:
@@ -86,4 +101,4 @@ class SQLiteService:
 
 
 if __name__ == "__main__":
-    SQLiteService()
+    print(SQLiteService().select_where_old_id(config.ADDRESS_DB_TABLE, 6781))
