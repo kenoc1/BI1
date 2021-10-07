b8a4181ff73eb9a7f74c654f2aa3f6ef7bf0f39b
Tobias <Telu8OjHyO2sWcnBSjfg>
2021-10-07 22:34:53 +0200

    feat: Lege SQLite-Datenbank für RUDOLF an

--- rudolf/sqlite_service.py
+++ rudolf/sqlite_service.py
@@ -1,4 +1,89 @@
+import sqlite3 as sl
+
+
 class SQLiteService:
 
     def __init__(self):
-        pass
+        self.con = sl.connect('rudolf-db.db')
+        self.setup()
+
+    def setup(self):
+        try:
+            with self.con:
+                self.con.execute("""
+                            CREATE TABLE if not exists adresse (
+                                adresse_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists bestellung (
+                                bestellung_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists einkauf (
+                                einkauf_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists lieferant_hersteller (
+                                lieferant_hersteller_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists marke (
+                                marke_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists produkte (
+                                produkte_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists kunden (
+                                produkte_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists mitarbeiter (
+                                mitarbeiter_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists subkategorien (
+                                subkategorien_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+                self.con.execute("""
+                            CREATE TABLE if not exists zwischenhaendler (
+                                zwischenhaendler_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                comb_id INTEGER,
+                                f2_id INTEGER
+                            );
+                        """)
+        except sl.OperationalError as oe:
+            print("DB Error")
+            print(oe)
+
+
+if __name__ == "__main__":
+    SQLiteService()
